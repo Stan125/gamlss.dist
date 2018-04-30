07b83ef2da0a438f8fa71355ca07d4e57dc8d420
Stan125 <stanislaus@stadlmann.cm>
2018-04-30 15:48:55 +0200

    Change GU and append the tests

--- tests/testthat/test_00_GU.R
+++ tests/testthat/test_00_GU.R
@@ -23,8 +23,8 @@ ex_emp <- round(mean(rvec), 2)
 vx_emp <- round(var(rvec), 2)
 
 ## Theoretical moments
-ex_theo <- round(fam$ex(mu, sigma), 2)
-vx_theo <- round(fam$vx(mu, sigma), 2)
+ex_theo <- round(fam$mean(mu, sigma), 2)
+vx_theo <- round(fam$variance(mu, sigma), 2)
 
 ## Test here
 expect_equal(ex_emp, ex_theo)
