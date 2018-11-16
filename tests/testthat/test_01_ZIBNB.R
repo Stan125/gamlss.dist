4407393c400d06a025e194f6f9ad70a1321f8ac6
JENScoding <jens.lichter@posteo.de>
2018-11-16 17:06:55 +0100

    Added moments and 2 tests for ZIBNB; Second test failed

---
+++ tests/testthat/test_01_ZIBNB.R
@@ -0,0 +1,37 @@
+#################################################
+############       gamlss.dist      #############
+############   Distribution tests   #############
+# Zero inflated beta negative binomial: ZIBNB() #
+#################################################
+
+## Context
+testthat::context("ZIBNB")
+
+## Seed
+set.seed(413)
+
+## Family
+fam <- ZIBNB()
+
+
+## Random Values
+i <- 0.5
+n <- 1e4
+mu <- 1 + i
+sigma <- 0.1 + i
+nu <- 1 + i
+tau <- 0.1 + i
+rvec <- rZIBNB(n, mu, sigma, nu, tau) 
+
+## Empirical Moments
+ex_emp <- mean(rvec)
+vx_emp <- var(rvec)
+
+## Theoretical moments
+ex_theo <- fam$mean(mu, sigma, nu, tau)
+vx_theo <- fam$variance(mu, sigma, nu, tau) 
+
+## Test here if they are about the same
+expect_true(abs(ex_emp - ex_theo) < 0.02)  
+expect_true(abs(vx_emp - vx_theo) < 0.02)  
+
