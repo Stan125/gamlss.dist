5d848bb53724dfaa065c7b884bacd843bd20bf75
JENScoding <jens.lichter@posteo.de>
2018-11-16 15:11:29 +0100

    Added moments and 2 tests for ZASICHEL

---
+++ tests/testthat/test_01_ZASICHEL.R
@@ -0,0 +1,39 @@
+#######################################
+#######       gamlss.dist      ########
+#######   Distribution tests   ########
+##  Zero adjusted Sichel: ZASICHEL() ##
+#######################################
+
+## Context
+testthat::context("ZASICHEL")
+
+## Seed
+set.seed(178)
+
+## Family
+fam <- ZASICHEL()
+
+## Random Values
+i <- 0.6
+n <- 1e4
+mu <- 1 + i
+sigma <- 1 + i  
+nu <- -0.5 + i
+tau <- 0.1 + i
+rvec <- rZASICHEL(n, mu, sigma, nu, tau) 
+
+## Empirical Moments
+ex_emp <- mean(rvec)
+vx_emp <- var(rvec)
+
+## Theoretical moments
+ex_theo <- fam$mean(mu, sigma, nu, tau)
+vx_theo <- fam$variance(mu, sigma, nu, tau) 
+
+## Test here if they are about the same
+expect_true(abs(ex_emp - ex_theo) < 0.02)  
+expect_true(abs(vx_emp - vx_theo) < 0.02)  
+
+
+
+
