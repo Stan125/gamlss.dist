4dd7b6ea4d023802cca33ab326a7cbd3ae2f1138
JENScoding <jens.lichter@posteo.de>
2018-11-16 23:55:35 +0100

    Second test for BE and BEo

---
+++ tests/testthat/test_01_BEo.R
@@ -0,0 +1,34 @@
+#########################################
+########     gamlss.dist        #########
+########   Distribution tests 2 #########
+##  Beta first parameterization: BEo() ##
+#########################################
+
+## Context
+testthat::context("BEo")
+
+## Seed
+set.seed(247)
+
+## Family
+fam <- BEo()
+
+## Random Values
+i <- 0.2
+n <- 1e6
+mu <- 0.5 + i
+sigma <- 0.2 + i
+rvec <- rBEo(n, mu, sigma) 
+
+## Empirical Moments
+ex_emp <- mean(rvec)
+vx_emp <- var(rvec)
+
+## Theoretical moments
+ex_theo <- fam$mean(mu, sigma)
+vx_theo <- fam$variance(mu, sigma)
+
+## Test here if they are about the same
+expect_true(abs(ex_emp - ex_theo) < 0.02)  
+expect_true(abs(vx_emp - vx_theo) < 0.02)
+
