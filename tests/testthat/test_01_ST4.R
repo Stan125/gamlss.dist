f17c735d41838203de483094da1e22c8a76b8acb
JENScoding <jens.lichter@posteo.de>
2018-11-19 20:12:05 +0100

    Correction of added moments for ST4 and second test for ST4

---
+++ tests/testthat/test_01_ST4.R
@@ -0,0 +1,38 @@
+#########################################
+#######     gamlss.dist        ##########
+#######   Distribution tests 2 ##########
+##### Skew Student t type 4: ST4() ######
+#########################################
+
+## Context
+testthat::context("ST4")
+
+## Seed
+set.seed(241)
+
+## Family
+fam <- ST4()
+
+## Random Values
+i <- 1
+n <- 1e6
+mu <- 0 + i
+sigma <- 1 + i
+nu <- 10 + i
+tau <- 10 + i
+rvec <- rST4(n, mu, sigma, nu, tau)  
+
+## Empirical Moments
+ex_emp <- mean(rvec)
+vx_emp <- var(rvec)
+
+## Theoretical moments
+ex_theo <- fam$mean(mu, sigma, nu, tau) 
+vx_theo <- fam$variance(mu, sigma, nu, tau)
+
+## Test here if they are about the same
+expect_true(abs(ex_emp - ex_theo) < 0.02)  
+expect_true(abs(vx_emp - vx_theo) < 0.02)
+
+
+
