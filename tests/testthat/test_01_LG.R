4a829e6fa1214ab2189b449358587a4455c06cbc
JENScoding <jens.lichter@posteo.de>
2018-11-19 22:36:47 +0100

    Second test for LG; FAILED

---
+++ tests/testthat/test_01_LG.R
@@ -0,0 +1,32 @@
+########################################
+#######     gamlss.dist        #########
+#######   Distribution tests 2 #########
+#######    Logarithmic: LG()   #########
+########################################
+
+## Context
+testthat::context("LG")
+
+## Seed
+set.seed(124)
+
+## Family
+fam <- LG()
+
+## Random Values
+i <- 0.2
+n <- 3e4
+mu <- 0.5 + i
+rvec <- rLG(n, mu)
+
+## Empirical Moments
+ex_emp <- mean(rvec)
+vx_emp <- var(rvec)
+
+## Theoretical moments
+ex_theo <- fam$mean(mu)
+vx_theo <- fam$variance(mu)
+
+## Test here if they are about the same
+expect_true(abs(ex_emp - ex_theo) < 0.02)  
+expect_true(abs(vx_emp - vx_theo) < 0.02)
