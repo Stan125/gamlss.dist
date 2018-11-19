9a4267ee367306089f7edc6662c1ced7cc904e7b
JENScoding <jens.lichter@posteo.de>
2018-11-19 21:05:56 +0100

    Second test for IG; FAILED

---
+++ tests/testthat/test_01_IG.R
@@ -0,0 +1,33 @@
+#######################################
+#######     gamlss.dist        ########
+#######   Distribution tests 2 ########
+######  Inverse Gaussian: IG()  #######
+#######################################
+
+## Context
+testthat::context("IG")
+
+## Seed
+set.seed(222)
+
+## Family
+fam <- IG()
+
+## Random Values
+i <- 0.5
+mu <- 1 + i
+sigma <- 1 + i
+n <- 1e4  
+rvec <- rIG(n, mu, sigma)
+
+## Empirical Moments
+ex_emp <- round(mean(rvec), 3)
+vx_emp <- round(var(rvec), 3)
+
+## Theoretical moments
+ex_theo <- round(fam$mean(mu, sigma),3)
+vx_theo <- round(fam$variance(mu, sigma),3)
+
+## Test here
+expect_true(abs(ex_emp - ex_theo) < 0.02)  
+expect_true(abs(vx_emp - vx_theo) < 0.02)  
