8b0b0c85cf8ddd4c951ec1ecef2e47a66b17c945
JENScoding <jens.lichter@posteo.de>
2018-11-19 12:23:32 +0100

    Correction of added moments for ST3 and second test for ST3; Test FAILED

---
+++ tests/testthat/test_01_ST3.R
@@ -0,0 +1,36 @@
+#########################################
+#######     gamlss.dist        ##########
+#######   Distribution tests 2 ##########
+##### Skew Student t type 3: ST3() ######
+#########################################
+
+## Context
+testthat::context("ST3")
+
+## Seed
+set.seed(241)
+
+## Family
+fam <- ST3()
+
+## Random Values
+i <- 1
+n <- 1e5
+mu <- 0 + i
+sigma <- 1 + i
+nu <- 1 + i
+tau <- 10 + i
+rvec <- rST3(n, mu, sigma, nu, tau) 
+
+## Empirical Moments
+ex_emp <- mean(rvec)
+vx_emp <- var(rvec)
+
+## Theoretical moments
+ex_theo <- fam$mean(mu, sigma, nu, tau)
+vx_theo <- fam$variance(mu, sigma, nu, tau)
+
+## Test here if they are about the same
+expect_true(abs(ex_emp - ex_theo) < 0.02)  
+expect_true(abs(vx_emp - vx_theo) < 0.02)
+
