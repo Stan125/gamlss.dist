b5d875ea6897a72ed3ec3dbc1fc5c2b6dc3bbdef
JENScoding <jens.lichter@posteo.de>
2018-11-19 23:03:56 +0100

    Second test for GPO; FAILED

---
+++ tests/testthat/test_01_GPO.R
@@ -0,0 +1,33 @@
+##################################
+#####     gamlss.dist        #####
+#####   Distribution tests 2 #####
+### Generalised Poisson: GPO() ###
+##################################
+
+## Context
+testthat::context("GPO")
+
+## Seed
+set.seed(218)
+
+## Family
+fam <- GPO()
+
+## Random Values
+i <- 0.5
+n <- 2e4
+mu <- 1 + i
+sigma <- 1 + i
+rvec <- rGPO(n, mu, sigma) 
+
+## Empirical Moments
+ex_emp <- mean(rvec)
+vx_emp <- var(rvec)
+
+## Theoretical moments
+ex_theo <- fam$mean(mu, sigma)
+vx_theo <- fam$variance(mu, sigma)
+
+## Test here if they are about the same
+expect_true(abs(ex_emp - ex_theo) < 0.02)  
+expect_true(abs(vx_emp - vx_theo) < 0.02) # test is not very robust
