d3afe82f1b2cd448861653c94e71bdb20f649521
JENScoding <jens.lichter@posteo.de>
2018-11-19 23:01:02 +0100

    Second test for ZIPF, NBI and NBII

---
+++ tests/testthat/test_01_ZIPF.R
@@ -0,0 +1,33 @@
+##################################
+#####     gamlss.dist        #####
+#####   Distribution tests 2 #####
+#####      Zipf: ZIPF()      #####
+##################################
+
+## Context
+testthat::context("ZIPF")
+
+## Seed
+set.seed(221)
+
+## Family
+fam <- ZIPF()
+
+## Random Values
+i <- 1
+n <- 1e4
+mu <- 5 + i
+rvec <- rZIPF(n, mu) 
+
+## Empirical Moments
+ex_emp <- mean(rvec)
+vx_emp <- var(rvec)
+
+## Theoretical moments
+ex_theo <- fam$mean(mu)
+vx_theo <- fam$variance(mu)
+
+## Test here if they are about the same
+expect_true(abs(ex_emp - ex_theo) < 0.02)  
+expect_true(abs(vx_emp - vx_theo) < 0.02)
+
